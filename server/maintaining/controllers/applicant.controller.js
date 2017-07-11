2019d58a84428084a4809e98856517c35be55dbb
mace fu <fireman88.ok@163.com>
2017-07-11 14:52:04 +0800

    complete applicant preview api

--- server/maintaining/controllers/applicant.controller.js
+++ server/maintaining/controllers/applicant.controller.js
@@ -1,4 +1,6 @@
 var _ = require('lodash');
+var mongoose = require('mongoose');
+var Applicant = mongoose.model('Applicant');
 
 exports.renderPreviewPage = renderPreviewPage;
 
@@ -10,7 +12,37 @@ function renderPreviewPage(req, res, next){
         name: '高晓松',
         gender: '男'
     };
-    res.render('./server/weChat/views/applicantPreview', {
-            applicant: data
+    if(_.isEmpty(applicantId)){
+        res.status(500).send({success: false, errmsg: 'applicant does not exist'});
+    } else {
+        Applicant.findOne({_id: applicantId}, function(err1, dbApplicant){
+            if(err1 || _.isEmpty(dbApplicant)) {
+                console.log('Error in finding applicant by id', applicantId, err1);
+                res.status(500).send({success: false, errmsg: 'Error in finding applicant'});
+            } else {
+                var app = {
+                    name: _.get(dbApplicant, ['name'], ''),
+                    gender: _.get(dbApplicant, ['gender'], ''),
+                    folk: _.get(dbApplicant, ['folk'], ''),
+                    birthDate: _.get(dbApplicant, ['birthDate'], ''),
+                    healthState: _.get(dbApplicant, ['healthState'], ''),
+                    idCardNumber: _.get(dbApplicant, ['idCardNumber'], ''),
+                    homeAddress: _.get(dbApplicant, ['homeAddress'], ''),
+                    currentAddress: _.get(dbApplicant, ['currentAddress'], ''),
+                    mobile: _.get(dbApplicant, ['mobile'], ''),
+                    tele: _.get(dbApplicant, ['tele'], ''),
+                    email: _.get(dbApplicant, ['email'], ''),
+                    qqNumber: _.get(dbApplicant, ['qqNumber'], ''),
+                    photoName: _.get(dbApplicant, ['photoName'], ''),
+                    idCardPhotoName: _.get(dbApplicant, ['idCardPhotoName'], ''),
+                    familyMembers: _.get(dbApplicant, ['familyMembers'], []),
+                    educationHistories: _.get(dbApplicant, ['educationHistories'], []),
+                    workExperiences: _.get(dbApplicant, ['workExperiences'], [])
+                }
+                res.render('./server/weChat/views/applicantPreview', {
+                    applicant: app
+                });
+            }
         })
+    }
 }
