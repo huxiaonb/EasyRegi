e7d7392ddc2b0f5661908afa1b813bb8f4880ac8
呼程广 <huchengguang@kingsoft.com>
2017-08-01 16:13:53 +0800

    add log to debug

--- mobile/src/components/company.js
+++ mobile/src/components/company.js
@@ -48,7 +48,8 @@ class Company extends React.Component{
                         "signType":"MD5",         //微信签名方式：     
                         "paySign":res.paySign //微信签名 
                     },
-                    function(res){     
+                    function(res){
+                        alert(JSON.stringify(res));   
                         if(res.err_msg == "get_brand_wcpay_request:ok" ) {
                             this.setState({
                                 payFlag : true
