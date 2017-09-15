4d2cdc8a9544f2ae13eff7bc82e237ec1f6fb030
呼程广 <huchengguang@kingsoft.com>
2017-09-15 09:36:26 +0800

    引入result

--- mobile/src/components/registerProfile/index.js
+++ mobile/src/components/registerProfile/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-import {Steps,Toast,Icon} from 'antd-mobile'
+import {Steps,Toast,Icon,Result} from 'antd-mobile'
 import moment from 'moment';
 import '../less/index.less'
 
