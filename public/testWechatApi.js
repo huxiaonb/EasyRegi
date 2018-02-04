2f9e896e3cdbacbdf480240a4f57fae8088a3a8b
mace fu <fireman88.ok@163.com>
2018-02-04 20:44:38 +0800

    add tips for sharing self-defined timeline

--- public/testWechatApi.js
+++ public/testWechatApi.js
@@ -1,34 +1,11 @@
 var openId = $('#openId').text();
-alert(openId);
 var signatureObj = JSON.parse($('#signatureObj').text());
-console.log(signatureObj.signature);
-function generateNonceString(length){
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var maxPos = chars.length;
-    var noceStr = "";
-    for (var i = 0; i < (length || 32); i++) {
-        noceStr += chars.charAt(Math.floor(Math.random() * maxPos));
-    }
-    return noceStr;
-};
-wx.config({
-    debug: true,
-    appId: 'wx54e94ab2ab199342',
-    timestamp: signatureObj.timestamp,//Date.now().toString().substr(0,10),
-    nonceStr: signatureObj.noncestr,//generateNonceString(),
-    signature: signatureObj.signature,
-    jsApiList: [
-        'checkJsApi',
-        'onMenuShareTimeline',
-        'onMenuShareAppMessage'
-    ]
-});
 
-wx.ready(function(){
+function shareToTimeLine(title, link, imgUrl){
     wx.onMenuShareTimeline({
-        title: '入职易--我刚找到了一份好工作，你也来试试',
-        link: 'http://www.mfca.com.cn',
-        imgUrl: 'http://www.mfca.com.cn/img/dog.jpg',
+        title: title,
+        link: link,
+        imgUrl: imgUrl,
         success: function () {
             // 用户确认分享后执行的回调函数
             alert('分享到朋友圈成功');
@@ -38,4 +15,88 @@ wx.ready(function(){
             alert('你没有分享到朋友圈');
         }
     });
+}
+
+$(document).ready(function(){
+    function bindClickEvent(){
+        $('#shareToTimeline').on('click', function(){
+            console.log('click');
+            WeixinJSBridge.invoke("shareTimeline",{
+                    "link":"http://www.mfca.com.cn",
+                    "img_url":"http://www.mfca.com.cn/img/dog.jpg",
+                    "img_width":"120",
+                    "img_height":"120",
+                    "desc":"i am description",
+                    "title":"just test from WeixinJsBridge"
+                },
+                function(e){
+                    alert(JSON.stringify(e));
+                });
+        });
+
+        $('#tipsForShare').on('click', function(){
+            $("#shareit").show();
+        });
+
+        $("#shareit").on("click", function(){
+            $("#shareit").hide();
+        });
+    }
+    /*function onBridgeReady(){
+        if(typeof WeixinJSBridge === "undefined") {
+            console.log('weixinJSBridge does not exist');
+            return;
+        }
+        WeixinJSBridge.on('menu:share:timeline', function(argv)
+        {
+            console.log('menu:share:timeline');
+            WeixinJSBridge.invoke("shareTimeline",{
+                    "link":"http://www.mfca.com.cn",
+                    "img_url":"http://www.mfca.com.cn/img/dog.jpg",
+                    "img_width":"120",
+                    "img_height":"120",
+                    "desc":"i am description",
+                    "title":"just test from WeixinJsBridge"
+                },
+                function(e){
+                    alert(JSON.stringify(e));
+                });
+        });
+    }
+    if(document.addEventListener){
+        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+    } else if(document.attachEvent){
+        document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+        document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+    }*/
+
+    bindClickEvent();
+
+});
+
+wx.config({
+    debug: true,
+    appId: 'wx54e94ab2ab199342',
+    timestamp: signatureObj.timestamp,//Date.now().toString().substr(0,10),
+    nonceStr: signatureObj.noncestr,//generateNonceString(),
+    signature: signatureObj.signature,
+    jsApiList: [
+        'checkJsApi',
+        'onMenuShareTimeline',
+        'onMenuShareAppMessage',
+        'hideOptionMenu',
+        'showOptionMenu',
+        'hideMenuItems',
+        'showMenuItems',
+        'hideAllNonBaseMenuItem',
+        'showAllNonBaseMenuItem',
+        'closeWindow'
+    ]
+});
+
+wx.ready(function(){
+    var title = '入职易--我刚找到了一份好工作，你也来试试',
+        link = 'http://www.mfca.com.cn',
+        imgUrl = 'http://www.mfca.com.cn/img/dog.jpg';
+    shareToTimeLine(title, link, imgUrl);
 });
