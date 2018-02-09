2d51e8a3930af0e034a826c63104d6b0683d1be1
呼程广 <huchengguang@kingsoft.com>
2018-02-09 18:04:33 +0800

    PC端 - 日常开发  提交

---
+++ maintain/src/components/resume.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+import {Button, Input, DatePicker, Table} from 'antd'
+
+export default class Resume extends React.Component {
+    render(){
+        return(
+            <div>這裡是簡歷池頁面</div>
+        )
+    }
+}
