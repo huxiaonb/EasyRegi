a6af1a00e8b67cdf5ee8333bf9c7bae2eb1cb3a9
呼程广 <huchengguang@kingsoft.com>
2018-03-22 16:33:25 +0800

    微简历编辑

--- mobile/src/components/basicInfo/step3.js
+++ mobile/src/components/basicInfo/step3.js
@@ -55,7 +55,7 @@ class Step3 extends React.Component{
                             {...getFieldProps('degree', {
                             rules:[{
                                 required:true, message:'请选择最高学历！'
-                            }],initialValue:info.highestDegree
+                            }],initialValue:[info.highestDegree]
                             })}
                             data={[{ label: '小学', value: '小学' }, { label:'初中',value:'初中'},{label:'高中',value:'高中'},{label:'大专',value:'大专'},{label:'本科',value:'本科'},{label:'硕士',value:'硕士'},{label:'博士',value:'博士'}]}
                             >
